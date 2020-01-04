80f31c28852754038ab720fdfa3eb8eba29a498c
geoboom <leegeorgie52@gmail.com>
2020-01-04 18:00:12 +0800

    Updates

---
+++ precache-manifest.56cd001300d9cea731009b340943a9bb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579eae52d0f6741754d53b801105e083",
+    "url": "/portfolio-v1/index.html"
+  },
+  {
+    "revision": "d5463d64e1fbe8d8474a",
+    "url": "/portfolio-v1/static/css/main.b126388b.chunk.css"
+  },
+  {
+    "revision": "5f143e0e05d1ac3d8175",
+    "url": "/portfolio-v1/static/js/2.9c5b5d4b.chunk.js"
+  },
+  {
+    "revision": "d1ea486fcee0578140d87e1d720e8e85",
+    "url": "/portfolio-v1/static/js/2.9c5b5d4b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5463d64e1fbe8d8474a",
+    "url": "/portfolio-v1/static/js/main.9c28e5be.chunk.js"
+  },
+  {
+    "revision": "ce0249468932595a4773",
+    "url": "/portfolio-v1/static/js/runtime-main.e6d07c50.js"
+  },
+  {
+    "revision": "1e4bb233c78c2a2841383b4cafc2d695",
+    "url": "/portfolio-v1/static/media/0002.1e4bb233.png"
+  },
+  {
+    "revision": "7c85b84c6b6eea27cffd2f8f1671c0d1",
+    "url": "/portfolio-v1/static/media/0002.7c85b84c.png"
+  },
+  {
+    "revision": "1f5ad41a4c4b344ab5816934663df53c",
+    "url": "/portfolio-v1/static/media/0004.1f5ad41a.png"
+  },
+  {
+    "revision": "9b019b59fa51950d11cc11e0139bc484",
+    "url": "/portfolio-v1/static/media/0005.9b019b59.png"
+  },
+  {
+    "revision": "18e4758b0cd709c24c61c94812a1b1bd",
+    "url": "/portfolio-v1/static/media/0006.18e4758b.png"
+  },
+  {
+    "revision": "d921a7a31b824fb188f69d3766c26823",
+    "url": "/portfolio-v1/static/media/0006.d921a7a3.png"
+  },
+  {
+    "revision": "f4dad8f2b1bb94bcdbe090a397222801",
+    "url": "/portfolio-v1/static/media/0007.f4dad8f2.png"
+  },
+  {
+    "revision": "95b1538e630f5af3d7e45191be636f60",
+    "url": "/portfolio-v1/static/media/0013.95b1538e.png"
+  },
+  {
+    "revision": "007d4687f5b74403f6c7bc152562eac1",
+    "url": "/portfolio-v1/static/media/0014.007d4687.png"
+  },
+  {
+    "revision": "992a7431c8bd9013832850eda5beae0c",
+    "url": "/portfolio-v1/static/media/1.992a7431.png"
+  },
+  {
+    "revision": "b36f0db5ca239e6fb83fae20453ae285",
+    "url": "/portfolio-v1/static/media/1.b36f0db5.png"
+  },
+  {
+    "revision": "ade81d19a4dc456322d87d43773977ec",
+    "url": "/portfolio-v1/static/media/2.ade81d19.png"
+  },
+  {
+    "revision": "05f47b1bf6f586367953bf78a07cb91a",
+    "url": "/portfolio-v1/static/media/3.05f47b1b.png"
+  }
+]);
